3853c86078e0d861b6c9af337a023a0f4e70f4df
Arkadiusz Pająk <arkad.paj@gmail.com>
2024-02-10 00:59:45 +0100

    średnia na cały rok i na II semestr

--- srednia_wazona.py
+++ srednia_wazona.py
@@ -1,26 +1,64 @@
-# https://i.imgur.com/njNBg1Z.png
-courses_ects = {
-'Język angielski': 4,
-'Matematyka dyskretna': 6,
-'Matematyka elementarna': 6,
-'Warsztat programisty': 4,
-'Wstęp do programowania': 7,
-'Wstęp do technologii Web': 3
+#
+# informacje o ocenach: https://i.imgur.com/njNBg1Z.png
+# informacje o stypendium: https://ug.edu.pl/studenci/stypendia-dla-studentow-i-doktorantow-szkol-doktorskich/stypendium-rektora
+#
+courses_ects_sem1 = {
+    'Język angielski I': 4,
+    'Matematyka dyskretna': 6,
+    'Matematyka elementarna': 6,
+    'Warsztat programisty': 4,
+    'Wstęp do programowania': 7,
+    'Wstęp do technologii Web': 3
 }
-weighted_avg = 0
-ects_sum = 0
-print('Podawaj oceny w następującym formacie: 5.0, 3.5, ...')
-for name in courses_ects.keys():
-    grade = input(f'Ocena z | {name} | ')
-    while grade not in ['5.0', '4.5', '4.0', '3.5', '3.0', '2.0']:
-        print('Niepoprawna ocena.')
+courses_ects_sem2 = {
+    'Język angielski II': 4,
+    'Algebra liniowa': 5,
+    'Algorytmy i struktury danych': 5,
+    'Bazy danych': 5,
+    'Języki programowania I': 4,
+    'Języki programowania II': 4,
+    'Matematyczne podstawy informatyki': 2
+}
+def oblicz_srednia(courses_ects):
+    grades_sum = 0
+    ects_sum = 0
+    print('Podawaj oceny w następującym formacie: 5.0, 3.5, ...')
+    for name in courses_ects.keys():
         grade = input(f'Ocena z | {name} | ')
-    ects = courses_ects[name]
-    ects_sum += ects
-    weighted_avg += ects * float(grade)
-
-print(f'Twoja średnia: {round(weighted_avg/ects_sum, 2)}')
-print('Minimalna średnia na stypendium w poprzednich latach: ')
-print('2022/23: 4.48')
-print('2021/22: 4.42')
-print('2020/21: 4.04')
+        while grade not in ['5.0', '4.5', '4.0', '3.5', '3.0', '2.0']:
+            print('Niepoprawna ocena.')
+            grade = input(f'Ocena z | {name} | ')
+        ects = courses_ects[name]
+        ects_sum += ects
+        grades_sum += ects * float(grade)
+    return grades_sum, ects_sum
+print("1. Oblicz średnią za 1. semestr")
+print("2. Oblicz średnią za 2. semestr")
+print("3. Oblicz średnią za 1. rok")
+wybor = input("? : ")
+if wybor not in ('1', '2','3'):
+    print("Nieprawidłowa opcja")
+elif wybor == '1':
+    grades_sum, ects_sum = oblicz_srednia(courses_ects_sem1)
+    print(f'Twoja średnia: {round(grades_sum / ects_sum, 2)}')
+    print('Minimalna średnia na stypendium w poprzednich latach: ')
+    print('2022/23: 4.48')
+    print('2021/22: 4.42')
+    print('2020/21: 4.04')
+elif wybor == '2':
+    grades_sum, ects_sum = oblicz_srednia(courses_ects_sem2)
+    print(f'Twoja średnia: {round(grades_sum / ects_sum, 2)}')
+    print('Minimalna średnia na stypendium w poprzednich latach: ')
+    print('2022/23: 4.48')
+    print('2021/22: 4.42')
+    print('2020/21: 4.04')
+else:
+    # Łączenie słownika z 2. semestru ze słownikem z 1. semestru
+    for key, value in courses_ects_sem2.items():
+        courses_ects_sem1[key] = value
+    grades_sum, ects_sum = oblicz_srednia(courses_ects_sem1)
+    print(f'Twoja średnia: {round(grades_sum / ects_sum, 2)}')
+    print('Minimalna średnia na stypendium w poprzednich latach: ')
+    print('2022/23: 4.48')
+    print('2021/22: 4.42')
+    print('2020/21: 4.04')
